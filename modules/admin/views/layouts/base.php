de2e54cec47a0f2be39925d1efef16f6f72bc621
Valery Kiseliou <v.a.kiselyov@gmail.com>
2018-01-23 23:38:07 +0300

    Create admin page - players

--- modules/admin/views/layouts/base.php
+++ modules/admin/views/layouts/base.php
@@ -20,11 +20,64 @@ AppAsset::register($this);
 	<?php $this->head() ?>
 </head>
     <body>
-    <?php $this->beginBody() ?>
 
-        <?= $content ?>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Iron-War</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <?=
+                \yii\widgets\Menu::widget([
+                    'options' => ['class' => 'nav navbar-nav navbar-right'],
+                    'items' => [
+                        ['label' => '<i class="glyphicon glyphicon-user"></i>', 'url' => ['site/index']],
+                        ['label' => '<i class="glyphicon glyphicon-log-in"></i>', 'url' => ['site/index']],
+                        ['label' => '<i class="glyphicon glyphicon-off"></i>', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                    ],
+                    'encodeLabels' => false,
+                    'activateParents' => true,
+                ]);
+            ?>
+            <form class="navbar-form navbar-right">
+                <input type="text" class="form-control" placeholder="Search...">
+            </form>
+        </div>
+    </div>
 
-    <?php $this->endBody() ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3 col-md-2 sidebar">
+                <?=
+                    \yii\widgets\Menu::widget([
+                        'options' => ['class' => 'nav nav-sidebar'],
+                        'items' => [
+                            ['label' => 'Home', 'url' => ['site/index']],
+                            ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                        ],
+                        'encodeLabels' => true,
+                        'activateParents' => true,
+                    ]);
+                ?>
+            </div>
+
+            <div class="col-sm-9 col-md-10">
+
+                <?php $this->beginBody() ?>
+
+                <?= $content ?>
+
+                <?php $this->endBody() ?>
+
+            </div>
+        </div>
+    </div>
+
     </body>
 </html>
 <?php $this->endPage() ?>
